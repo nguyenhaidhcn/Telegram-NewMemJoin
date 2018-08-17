0bec58430df249540117d639f1b3362c4e84555a
haint <nguyenhaidhcn@gmail.com>
2018-08-17 10:13:11 +0700

    save use to DB

---
+++ src/main/java/telegram/UserDB.java
@@ -0,0 +1,40 @@
+package telegram;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+public class UserDB {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Integer user_id; ///< Unique identifier for this user or bot
+    public String firstName; ///< User‘s or bot’s first name
+    public Boolean isBot; ///< True, if this user is a bot
+    public String lastName; ///< Optional. User‘s or bot’s last name
+    public String userName; ///< Optional. User‘s or bot’s username
+    public String languageCode; ///< Optional. IETF language tag of the user's language
+
+    public UserDB() {
+    }
+
+    @Override
+    public String toString() {
+        return "UserDB{" +
+                "user_id=" + user_id +
+                ", firstName='" + firstName + '\'' +
+                ", isBot=" + isBot +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", languageCode='" + languageCode + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/telegram/UserRepository.java
@@ -0,0 +1,13 @@
+package telegram;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by rajeevkumarsingh on 27/06/17.
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<UserDB, Long> {
+
+}
