f474ebc4d1eb472c6011f61d7099f502f8852770
haint <nguyenhaidhcn@gmail.com>
2018-08-20 12:52:59 +0700

    only user name

--- src/main/java/com/telegram/TelegramBot.java
+++ src/main/java/com/telegram/TelegramBot.java
@@ -63,10 +63,10 @@ public class TelegramBot extends TelegramLongPollingBot  {
 
 
                    UserDB userDB = new UserDB();
-                   userDB.first_name = user.getFirstName();
-                   userDB.last_name = user.getLastName();
+//                   userDB.first_name = user.getFirstName();
+//                   userDB.last_name = user.getLastName();
                    userDB.user_name = user.getUserName();
-                   userDB.user_id = user.getId();
+//                   userDB.user_id = user.getId();
                    Save(userDB);
 
 
@@ -105,8 +105,8 @@ public class TelegramBot extends TelegramLongPollingBot  {
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json");
 
-            String input = "{\"user_id\":%d,\"first_name\":\"%s\", \"last_name\":\"%s\", \"user_name\":\"%s\", \"languageCode\":\"%s\"}";;
-            input = String.format(input, userDB.user_id, userDB.first_name, userDB.last_name,userDB.user_name, userDB.language_code);
+            String input = "{\"user_name\":\"%s\"}";;
+            input = String.format(input,userDB.user_name);
 
             OutputStream os = conn.getOutputStream();
             os.write(input.getBytes());
--- src/main/java/com/telegram/UserDB.java
+++ src/main/java/com/telegram/UserDB.java
@@ -13,12 +13,12 @@ public class UserDB {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Integer user_id; ///< Unique identifier for this user or bot
-    public String first_name; ///< User‘s or bot’s first name
+//    public Integer user_id; ///< Unique identifier for this user or bot
+//    public String first_name; ///< User‘s or bot’s first name
     public Boolean is_bot; ///< True, if this user is a bot
-    public String last_name; ///< Optional. User‘s or bot’s last name
+//    public String last_name; ///< Optional. User‘s or bot’s last name
     public String user_name; ///< Optional. User‘s or bot’s username
-    public String language_code; ///< Optional. IETF language tag of the user's language
+//    public String language_code; ///< Optional. IETF language tag of the user's language
 
     public UserDB() {
     }
